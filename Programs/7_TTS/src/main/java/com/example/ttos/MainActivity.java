c155baa29733e3abea04c6a01d8b696e14650d29
Nidhi Charate <nchat2002@gmail.com>
2023-06-25 11:57:10 +0530

    7_TTS

---
+++ Programs/7_TTS/src/main/java/com/example/ttos/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ttos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText txtSpeak;
+    Button btnSpeak;
+    TextToSpeech textToSpeech;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtSpeak=(EditText)findViewById(R.id.editText);
+        btnSpeak=(Button)findViewById(R.id.btn_speak);
+        btnSpeak.setOnClickListener(this);
+        textToSpeech=new TextToSpeech(getBaseContext(),
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status!=TextToSpeech.ERROR)
+                        {
+                            Toast.makeText(getBaseContext(),
+                                    "Success",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    public void onClick(View v)
+    {
+        String text=txtSpeak.getText().toString();
+        textToSpeech.speak(text,
+                TextToSpeech.QUEUE_FLUSH,null);
+    }
+}
